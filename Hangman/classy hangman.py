ef1589505790a7d6c60d5c4827f83361ca6a4439
Alexandru Marton <marton.alexandru@gmail.com>
2020-08-11 19:42:18 -0400

    updates to classy hangman

--- Hangman/classy hangman.py
+++ Hangman/classy hangman.py
@@ -1,13 +1,14 @@
 #%%
 #Hangman python class
 
-from hangman import bad_guesses, good_guesses, new_game_word, possible_words
+from Hangman.hangman import good_guesses, user_guess
 import random
 
 class Hangman():
     def __init__(self) -> None:
         self.possible_words = ["water", "amalgamation", "democracy", "president", "logistic", "headache", "computation"]
-        self.guess = new_game_word()
+        self.to_guess = new_game_word()
+        self.u_guess = user_guess()
         self.word_display
         self.bad_guesses = []
         self.good_guesses = []
@@ -15,11 +16,11 @@ class Hangman():
         self.lives = 10
 
     def new_game_word(self):
-        to_guess = random.choice(self.possible_words)
+        new_word = random.choice(self.possible_words)
         print("The word has been chosen. Now you must guess it")
-        return to_guess
+        return new_word
     
-    def user_guess(self):
+    def user_guess(self, good_guesses, bad_guesses):
         print("A guess must be made soon!")
         valid_input = False
         while valid_input == False:
@@ -30,7 +31,7 @@ class Hangman():
                 print(f"Your previous guesses were: {already_guessed}")
                 valid_input = False
             else:
-                if user_input in self.guess:
+                if user_input in self.to_guess:
                     print(f"Your guess {user_input} was correct!")
                     good_guesses.append(user_input)
                     valid_input = True
@@ -40,18 +41,18 @@ class Hangman():
                     self.wrong_attempts += 1
                     valid_input = True
     
-    def display_word(self):
-        for letter_position in range(len(new_game_word)):
-            if new_game_word[letter_position] in good_guesses:
+    def display_word(self, to_guess, good_guesses):
+        for letter_position in range(len(to_guess)):
+            if to_guess[letter_position] in good_guesses:
                 temp_list = list(self.word_display)
-                temp_list[letter_position] = new_game_word[letter_position]
+                temp_list[letter_position] = to_guess[letter_position]
                 self.word_display = str(temp_list)
         print(f"Your guesses show {self.word_display} so far!")
         return self.display_word
 
-    def game_over(self):
-        if self.guess == self.word_display:
-            print(f"Congratulations, you guessed {new_game_word} and get to see another day!")
+    def game_over(self, u_guess):
+        if self.to_guess == self.word_display:
+            print(f"Congratulations, you guessed {u_guess} and get to see another day!")
             game_over = True
             return game_over
         elif self.wrong_attempts == self.lives + 1:
