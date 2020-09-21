fbf31f6ac9688a215b9d2daffe6e4beed949da1d
Alexandru Marton <marton.alexandru@gmail.com>
2020-09-21 13:32:32 -0400

    Satements + Variables added

--- Accounting/Accounting.py
+++ Accounting/Accounting.py
@@ -6,24 +6,28 @@ import datetime
 Equity = 0
 Assets = 0
 Liabilities = 0
-CommonStock = 1000
+OpeningContributedCapital = 1000
+IssuedShares = 0
+RepurchasedShares = 0
 RetainedEarnings = 0
+OpeningRetainedEarnings = 0
+NetEarnings = 0
 Revenues = 0
 Expenses = 0
 Dividends = 0
 
-#Equity = Assets - Liabilities
+def Statement_of_Earnings(Revenues, Expenses):
+    NetEarnings = Revenues - Expenses
+    return NetEarnings
 
-def FinalAssets(Equity, Liabilities, CommonStock, Revenues, Expenses, Dividends):
-    RetainedEarnings = Revenues - Expenses - Dividends
-    Equity = CommonStock + RetainedEarnings
-    Assets = Equity + Liabilities
-    print(f"Assets at are equal to {Assets}")
-    return Assets
+def Statement_of_Changes_in_Equity(OpeningContributedCapital, IssuedShares, RepurchasedShares):
+    ContributedCapital = OpeningContributedCapital + IssuedShares - RepurchasedShares
+    RetainedEarnings = OpeningRetainedEarnings + NetEarnings - Dividends
+    Equity = ContributedCapital + RetainedEarnings
+    return Equity
 
-Assets = FinalAssets(Equity, Liabilities, CommonStock, Revenues, Expenses, Dividends)
-
-print(Assets)
+def Satement_of_Financial_Position(Assets, Liabilities, Equity):
+    assert Assets == Equity + Liabilities
 
 
 # %%
